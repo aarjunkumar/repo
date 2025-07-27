cb766519f53dba7de6f3b92bac19765aabbe5a41
aarjunkumar <aarrjunkkumar619@gmail.com>
2025-07-27 11:04:41 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+import google.generativeai as genai
+import os 
+import streamlit as st 
+
+os.environ["GOOGLe_API_KEY"]="AIzaSyCX4gQ8qjCLShyfeRVJfn_Y6vJBZ93VHUo"
+genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
+model=genai.GenerativeModel("gemini-2.0-flash")
+
+st.title("AI Search")
+input_text=st.text_input("Search the topic you want")
+
+if input_text:
+  response=model.generate_content(input_text)
+  st.write(response.text)
+
+## http://192.168.29.104:8501
